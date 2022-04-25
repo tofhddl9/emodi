a197ab207b8124fe1bb16cd1496c7b29ed34b736
tofhddl9 <tofhddl9@gmail.com>
2022-04-26 01:37:39 +0900

    Add CalendarLayout for fast navigation

--- app/src/main/java/com/lgtm/emoji_diary/MainFragment.kt
+++ app/src/main/java/com/lgtm/emoji_diary/MainFragment.kt
@@ -2,6 +2,7 @@ package com.lgtm.emoji_diary
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.haibin.calendarview.Calendar
 import com.haibin.calendarview.CalendarView
@@ -9,34 +10,64 @@ import com.lgtm.emoji_diary.databinding.FragmentMainBinding
 import com.lgtm.emoji_diary.delegate.viewBinding
 import com.lgtm.emoji_diary.utils.CalendarUtil
 
-class MainFragment: Fragment(R.layout.fragment_main), CalendarView.OnCalendarSelectListener {
+class MainFragment : Fragment(R.layout.fragment_main), CalendarView.OnCalendarSelectListener, CalendarView.OnYearChangeListener {
 
     private val binding: FragmentMainBinding by viewBinding(FragmentMainBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.calendarView.setOnCalendarSelectListener(this)
-
         initViews()
+
+        setListeners()
     }
 
     private fun initViews() = with(binding) {
         yearView.text = calendarView.curYear.toString()
         monthView.text = CalendarUtil.monthInEnglish(calendarView.curMonth)
+
+//        val calendar = Calendar().apply {
+//            year = 2022
+//            month = 2
+//            day = 22
+//        }
+//        calendarView.addSchemeDate(calendar)
+//        calendarView.addSchemeDate(mapOf("id" to calendar))
+//        db 데이터를 가공해서 이런식으로 캘린더뷰에 연동
     }
 
-    override fun onCalendarOutOfRange(calendar: Calendar?) {
-        // do nothing
+    private fun setListeners() {
+        binding.calendarView.setOnCalendarSelectListener(this)
+
+        binding.calendarView.setOnYearChangeListener(this)
+
+        // TODO : Animation
+        binding.yearView.setOnClickListener {
+            val currentYear = binding.calendarView.curYear
+            binding.calendarView.showYearSelectLayout(currentYear)
+            binding.yearView.text = currentYear.toString()
+
+            binding.monthView.isVisible = false
+        }
     }
 
     override fun onCalendarSelect(calendar: Calendar, isClick: Boolean) {
         updateView(calendar)
     }
 
+    override fun onCalendarOutOfRange(calendar: Calendar?) {
+        // do nothing
+    }
+
     private fun updateView(calendar: Calendar) = with(binding) {
         yearView.text = calendar.year.toString()
-       monthView.text = CalendarUtil.monthInEnglish(calendar.month)
+        monthView.text = CalendarUtil.monthInEnglish(calendar.month)
+
+        monthView.isVisible = true
+    }
+
+    override fun onYearChange(year: Int) = with(binding) {
+        yearView.text = year.toString()
     }
 
 }
